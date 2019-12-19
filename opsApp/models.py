1bd4799927fcf8a688a2cd8f032c121e128d52b0
hatwhite89 <@Tatiana1987>
2019-12-18 18:03:25 -0600

    la loca esta loca

--- opsApp/models.py
+++ opsApp/models.py
@@ -53,6 +53,9 @@ class ViasDeAdministracion(models.Model):
       nombre_via_administracion = models.CharField(max_length=200,null=True,blank=True)
       fecha_creacion = models.DateField(null=True)
       def __str__(self):
+            if (self.nombre_via_administracion == ''):
+                  return ""
+
             return self.nombre_via_administracion
 
 class Medicamento(models.Model):
